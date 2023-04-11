b380df5709bb7836beb9a783e3e8144468dd99ef
Diwakar <diwakar.raja@ymedialabs.com>
2023-04-11 07:24:13 -0700

    Adding request animation frame and ref to fix performance issue

--- src/Tabs/Tabs.js
+++ src/Tabs/Tabs.js
@@ -1,27 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Tabs.css';
 
 const Tabs = ({ labels, onTabClick, activeTabIndex }) => {
-    const [progress, setProgress] = useState(0);
+    const progressBarRef = useRef(null);
+    const animationFrameRef = useRef(null);
+
+    const animateProgressBar = () => {
+        const startTime = performance.now();
+        const totalDuration = 3000; // 3 seconds
+
+        const animate = (currentTime) => {
+            const elapsedTime = currentTime - startTime;
+            const progressPercentage = (elapsedTime / totalDuration) * 100;
+
+            if (progressPercentage < 100) {
+                progressBarRef.current.style.height = `${progressPercentage}%`;
+                animationFrameRef.current = requestAnimationFrame(animate);
+            } else {
+                progressBarRef.current.style.height = '100%';
+            }
+        };
+
+        progressBarRef.current.style.height = '0%';
+        animationFrameRef.current = requestAnimationFrame(animate);
+    };
 
     useEffect(() => {
-        setProgress(0);
-
-        const totalIntervalTime = 3000; // 3 seconds
-        const timerInterval = 100; // 100ms
-        const progressIncrement = 100 / (totalIntervalTime / timerInterval);
-
-        const interval = setInterval(() => {
-            setProgress((prevProgress) => {
-                if (prevProgress < 100) {
-                    return prevProgress + progressIncrement;
-                }
-                return prevProgress;
-            });
-        }, timerInterval);
+        animateProgressBar();
 
         return () => {
-            clearInterval(interval);
+            cancelAnimationFrame(animationFrameRef.current);
         };
     }, [activeTabIndex]);
 
@@ -31,18 +39,21 @@ const Tabs = ({ labels, onTabClick, activeTabIndex }) => {
                 <div key={index} className="tab">
                     <button
                         className={`tab-button${index === activeTabIndex ? ' active' : ''}`}
-                        onClick={() => onTabClick(index)}
+                        onClick={() => {
+                            onTabClick(index);
+                            animateProgressBar();
+                        }}
                     >
                         {label}
                     </button>
-                    {index === activeTabIndex ? (
+                    {index === activeTabIndex && (
                         <div
+                            ref={progressBarRef}
                             className="progress-bar"
-                            style={{ height: `${progress}%` }}
+                            style={{ height: '0%', transition: 'height 0s' }}
                         ></div>
-                    ) : (
-                        <div className="dot-icon"></div>
                     )}
+                    {index !== activeTabIndex && <div className="dot-icon"></div>}
                 </div>
             ))}
         </div>
